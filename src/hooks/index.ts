2a1d7386af524ee60bb643a6f95e8f90ace0c10f
shun kong cheung <shunkong.cheung@gmail.com>
2020-03-11 21:48:20 -0400

    feat(useFetchDelete): init

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,15 +1,17 @@
 import useDeferredValue from "./useDeferredValue";
 import useFakeFetchDetail from "./useFakeFetchDetail";
 import useFakeFetchList from "./useFakeFetchList";
-import useRestFetch from "./useRestFetch";
+import useFetchDelete from "./useFetchDelete";
 import useFetchDetail from "./useFetchDetail";
 import useFetchEdit from "./useFetchEdit";
 import useFetchList from "./useFetchList";
+import useRestFetch from "./useRestFetch";
 
 export {
   useDeferredValue,
   useFakeFetchDetail,
   useFakeFetchList,
+  useFetchDelete,
   useFetchDetail,
   useFetchEdit,
   useFetchList,
